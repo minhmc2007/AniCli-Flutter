d1128faade538fb168332170cc6afa100738a5cd
MINHMC2077 <106906918+minhmc2007@users.noreply.github.com>
2026-07-08 23:59:41 +0700

    Change app version to 1.8.9
    Updated app version to 1.8.9.

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 // Global Definitions & App State
-const String kAppVersion = "1.8.9 Open Beta";
+const String kAppVersion = "1.8.9";
 const String kBuildNumber = "189";
 const kColorCream = Color(0xFFFEEAC9);
 const kColorPeach = Color(0xFFFFCDC9);
